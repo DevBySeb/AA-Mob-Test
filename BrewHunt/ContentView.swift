ab396258fc185d7cc8d168a28e7653c57fbcaf3e
Samet Berberoglu <sametberberoglu35@gmail.com>
2025-06-10 23:30:52 +0200

    Initial commit of the project

---
+++ BrewHunt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrewHunt
+//
+//  Created by Samet Berberoğlu on 2025-06-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
